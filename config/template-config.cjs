cf6e478d6ac2c68cf515b0bcd72130076428e790
bijingheouren <57903756+bijingheouren@users.noreply.github.com>
2024-01-12 02:47:13 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -14,30 +14,26 @@ const TEMPLATE_CONFIG = [
       
       天气：{{weather.DATA}}
       
-      温度: {{min_temperature.DATA}}--{{max_temperature.DATA}}
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
       
       风级: {{wind_scale.DATA}}
-
-      温馨提示：{{notice.DATA}}
       
       {{comprehensive_horoscope.DATA}}
       
       ---
-      每日一句
-       ：{{earthy_love_words.DATA}}
-     
-
-      中文：{{note_ch.DATA}}
-      
-      English：{{note_en.DATA}}
       
       今天是我们相识的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
       ---
+      
+      {{moment_copyrighting.DATA}}
+      
+      {{poetry_title.DATA}} {{poetry_content.DATA}}
+    `
   },
   {
     id: '0002',
