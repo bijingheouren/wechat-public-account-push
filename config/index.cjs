70caced430ef744dabab7c601130cf6b75dbc883
bijingheouren <57903756+bijingheouren@users.noreply.github.com>
2024-01-12 01:25:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-CgP6w-TzoUqRI1q6CrvcP0J3hI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'AZneIajduNxgDktD56Jyv7ve0t4hN1jfBwmSZnXmnaA',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-29',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'AFYiAruxDOGyYPMfnfUv9sDVGwrdt89Gmg-vO6GBeXY',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
